8c2e1c1680572a39d4c80ac784085c72b4d8b394
Николай Котко <kalb9n4ik@gmail.com>
2023-07-10 22:42:22 +0300

    Second commit slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,60 +3,66 @@ const buttonNext = document.querySelector('.slider__arrow_next')
 let sliderItems = document.querySelectorAll('.slider__items > div')
 let sliderDots = document.querySelectorAll('.slider__dots > div')
 
-let direction = null;
-setSliderDot()
 
+// Установка начальной точки
+sliderDots[0].classList.toggle('slider__dot_active')
+
+for (let i = 0; i < sliderDots.length; i++) {
+	sliderDots[i].onclick = () => {
+		const oldIndex = getOldImage();
+		changeImageForDots(i, oldIndex)
+		setSliderDot(i, oldIndex)
+		
+	}
+}
 
 buttonPrev.onclick = () => {
-	direction = 0;
-	changeImage(direction);
+	const old = getOldImage()
+	prevImage(old)
 }
 
 buttonNext.onclick = () => {
-	direction = 1;
-	changeImage(direction);
+	const old = getOldImage()
+	nextImage(old)
 }
 
-function changeImage(direction) {
+function getOldImage(){
 	const arrImages = Array.from(sliderItems)
 	const indexOldImage = arrImages.indexOf(document.querySelector('div.slider__item_active'))
-
-	sliderItems[indexOldImage].classList.toggle('slider__item_active')
-
-
-	if(direction) {
-	//	console.log('Нажали вперед')
-		if(indexOldImage ===  arrImages.length - 1) {
-			sliderItems[0].classList.toggle('slider__item_active')
-			setSliderDot(0, indexOldImage)
-			
-		} else {
-			sliderItems[indexOldImage + 1].classList.toggle('slider__item_active')
-			setSliderDot(indexOldImage + 1, indexOldImage)
-		}
-		
-	} else	{
-	//	console.log('Нажали назад')
-		if(indexOldImage ===  0) {
-			sliderItems[arrImages.length - 1].classList.toggle('slider__item_active')
-			setSliderDot(arrImages.length - 1, indexOldImage)
-		} else {
-			sliderItems[indexOldImage - 1].classList.toggle('slider__item_active')
-			setSliderDot(indexOldImage - 1, indexOldImage)
-		}
-	}
+	return indexOldImage
 }
 
 
-function setSliderDot(indexNew, indexOld) {
+function nextImage(indexOldImage){
+	sliderItems[indexOldImage].classList.toggle('slider__item_active')
 
-	// Установка начальной точки
-	if(direction === null){
-		sliderDots[0].classList.toggle('slider__dot_active')
-		return
+	if(indexOldImage ===  sliderItems.length - 1) {
+		sliderItems[0].classList.toggle('slider__item_active')
+		setSliderDot(0, indexOldImage)
+	} else {
+		sliderItems[indexOldImage + 1].classList.toggle('slider__item_active')
+		setSliderDot(indexOldImage + 1, indexOldImage)
 	}
+}
 
+function prevImage(indexOldImage){
+	sliderItems[indexOldImage].classList.toggle('slider__item_active')
 
-	sliderDots[indexOld].classList.toggle('slider__dot_active')
-	sliderDots[indexNew].classList.toggle('slider__dot_active')
+	if(indexOldImage ===  0) {
+		sliderItems[sliderItems.length - 1].classList.toggle('slider__item_active')
+		setSliderDot(sliderItems.length - 1, indexOldImage)
+	} else {
+		sliderItems[indexOldImage - 1].classList.toggle('slider__item_active')
+		setSliderDot(indexOldImage - 1, indexOldImage)
+	}
+}
+
+function setSliderDot(newIndex, oldIndex){
+	sliderDots[newIndex].classList.toggle('slider__dot_active')
+	sliderDots[oldIndex].classList.toggle('slider__dot_active')
+}
+
+function changeImageForDots(newIndex, oldIndex){
+	sliderItems[oldIndex].classList.toggle('slider__item_active')
+	sliderItems[newIndex].classList.toggle('slider__item_active')
 }
