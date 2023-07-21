1a13dd149900a3891badc12c595b8d1aa2422c9c
Николай Котко <kalb9n4ik@gmail.com>
2023-07-21 19:31:02 +0300

    Second commit dropdown

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,18 +1,32 @@
-const dropDownValue = document.querySelector('.dropdown__value')
-const dropDown = dropDownValue.closest('.dropdown')
-const list = dropDown.querySelector('ul')
+const dropDownList = document.querySelectorAll('.dropdown')
+const array = Array.from(dropDownList)
 
-dropDown.addEventListener('click', openList)
+for (let i = 0; i < array.length; i++) {
+	//console.log(array[i].children)
+	array[i].querySelector('div').addEventListener('click', toggleDropDown)
+	array[i].querySelector('ul').addEventListener('click', changeDropDownVale)
+}
 
 
-function openList(e) {
+// function openList(e) {
+// 	const dropDownValue = this.querySelector('.dropdown__value')
+// 	const list = this.closest('.dropdown').querySelector('ul')
 
+// 	if(list.classList.value.includes('dropdown__list_active')){
+// 		dropDownValue.textContent =  e.target.textContent;
+
+// 	}
+// 	e.preventDefault();
+// 	list.classList.toggle('dropdown__list_active')
+	
+// }
+function toggleDropDown() {
+	//console.log(this.closest('.dropdown'))
+	this.closest('.dropdown').querySelector('ul').classList.toggle('dropdown__list_active')
+}
+
+function changeDropDownVale(e) {
 	e.preventDefault();
-	
-	if(list.classList.value.includes('dropdown__list_active')) {
-		console.log('выключили')
-		dropDownValue.textContent = e.target.textContent
-	}
-	
-	list.classList.toggle('dropdown__list_active')
+	this.closest('.dropdown').querySelector('div').textContent = e.target.textContent
+	this.closest('.dropdown').querySelector('ul').classList.toggle('dropdown__list_active')
 }
