8a9432b89ab9ba15899a1a738c2feff38ce31f87
Николай Котко <kalb9n4ik@gmail.com>
2023-08-01 16:51:26 +0300

    Second commit auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,15 +1,12 @@
 const form = document.forms['signin__form']
 const signoutBtn = document.getElementById('signout__btn')
 
+welcomeUser(localStorage.getItem('idUser'))
+
 signoutBtn.addEventListener('click', () => {
     delete localStorage.idUser
     welcomeUser()
-
 })
-
-if(localStorage.getItem('idUser')){
-    welcomeUser(localStorage.getItem('idUser'))
-}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -25,44 +22,33 @@ form.addEventListener('submit', (e) => {
 
 function sendXHR(){
     let xhr = new XMLHttpRequest()
-    xhr.addEventListener('readystatechange', () => {
-        if(xhr.readyState === xhr.DONE){
-            parseJson(xhr.responseText)
+    xhr.addEventListener('load', () => {
+        // parseJson(xhr.responseText)
+        if(xhr.response.success){
+            localStorage.setItem('idUser', xhr.response['user_id'])
+            welcomeUser(xhr.response['user_id'])
+        } else {
+            alert('Неверный логин/пароль')
         }
+        form.reset()
     })
 
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth')
     const formData = new FormData(form)
+    xhr.responseType = 'json';
     xhr.send(formData);
-}
 
-function parseJson(json){
-    const answer = JSON.parse(json)
-    if(answer.success){
-        localStorage.setItem('idUser', answer['user_id'])
-        welcomeUser(answer['user_id'])
-    } else {
-        alert('Неверный логин/пароль')
-    }
-    formClear()
 }
 
 function welcomeUser(idUser){
-    const signin = document.getElementById('signin')
-    const welcome = document.getElementById('welcome')
-    const userId = document.getElementById('user_id')
-    signin.classList.toggle('signin_active')
+    if(idUser !== null) {
+        const signin = document.getElementById('signin')
+        const welcome = document.getElementById('welcome')
+        const userId = document.getElementById('user_id')
+        
+        signin.classList.toggle('signin_active')
+        welcome.classList.toggle('welcome_active')
 
-    if(idUser){
         userId.textContent = idUser
-    }
-
-    welcome.classList.toggle('welcome_active')
-}
-
-function formClear(){
-    const inputs = document.getElementsByTagName('input')
-    for(let input of inputs){
-        input.value = ''
     }
 }
